bf561310fd1a9e676dc26dbba5926106c39de6ae
musti22git <mustafakhateb@hotmail.com>
2020-09-30 16:20:59 +0200

    tilføjet spiller startpoint og terningens attributer.

--- src/main/java/com/company/Game.java
+++ src/main/java/com/company/Game.java
@@ -4,6 +4,8 @@ package com.company;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
 
+import java.util.Random;
+
 public class Game {
 
     public static void main(String[] args) {
@@ -20,5 +22,19 @@ public class Game {
 
         gui.addPlayer(gui_player_1);
         gui.addPlayer(gui_player_2);
+
+        //Setting player startpoints
+        gui_Player_1.setBalance(0);
+        gui_Player_2.setBalance(0);
+
+        //Dice attributes
+        Random rand = new Random();
+        int diceRoll_1, diceRoll_2, diceSum;
+        int min=1;
+        int max=6;
+        int player1_points = 0;
+        int player2_points = 0;
+        int turn = 0;
+
     }
 }
