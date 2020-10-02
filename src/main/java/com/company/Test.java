506644b4cb137544a19532e621cf3f6276444ec9
Adnan <ligeiprutten123@gmail.com>
2020-10-02 10:00:53 +0200

    Vi har lige lavet en test der viser den mest sandsynlige værdi for terninge kastet

--- src/main/java/com/company/Test.java
+++ src/main/java/com/company/Test.java
@@ -1,11 +1,31 @@
 package com.company;
 
-
+import java.util.Random;
 import gui_main.GUI;
 
 public class Test {
     public static void main(String[] args) {
         GUI gui = new GUI();
         gui.setDice(3, 4);
+        Random rand = new Random();
+        int diceRoll_1, diceRoll_2, diceSum;
+        int min=1;
+        int max=6;
+        int diceThrows = 0;
+        int diceSumAvg = 0;
+
+        while (diceThrows <= 10000){
+            diceRoll_1 = rand.nextInt(max) + min; // Rolling dice 1
+            diceRoll_2 = rand.nextInt(max) + min; // Rolling dice 2
+
+            diceSum = diceRoll_1 + diceRoll_2; // Calculating dice sum
+
+            diceSumAvg = diceSumAvg + diceSum;
+
+            diceThrows++;
+        }
+        System.out.println(diceSumAvg/10000.0);
     }
+
+
 }
